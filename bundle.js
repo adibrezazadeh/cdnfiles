cf2820c3d4549c2558f7010bf03b2256878ab282
adib rezazadeh <adib.rezazadeh@gmail.com>
2024-10-09 16:20:56 +0330

    hii

--- bundle.js
+++ bundle.js
@@ -1,14 +1,17 @@
 var MyComponent = (function () {
-  "use strict";
-  return function () {
-    return React.createElement(
-      React.Fragment,
-      null,
-      React.createElement(
-        "h1",
+    "use strict";
+    return function () {
+      return React.createElement(
+        React.Fragment,
         null,
-        "hiasdsakdjadsalkfhjksadlfhsadjklghsdjlgk"
-      )
-    );
-  };
-})();
+        React.createElement(
+          "h1",
+          null,
+          "hiasdsakdjadsalkfhjksadlfhsadjklghsdjlgk"
+        )
+      );
+    };
+  })();
+  
+  // مستند کردن MyComponent به شیء window
+  window.MyComponent = MyComponent;
