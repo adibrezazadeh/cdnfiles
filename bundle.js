d99b0de132b0b9c7cae3f99c9985d6476d58b228
adib rezazadeh <adib.rezazadeh@gmail.com>
2024-10-13 08:55:35 +0330

    new Cdn

--- bundle.js
+++ bundle.js
@@ -1,17 +1,19 @@
-var MyComponent = (function () {
-    "use strict";
-    return function () {
-      return React.createElement(
-        React.Fragment,
-        null,
-        React.createElement(
-          "h1",
-          null,
-          "hiasdsakdjadsalkfhjksadlfhsadjklghsdjlgk"
-        )
-      );
-    };
-  })();
-  
-  // مستند کردن MyComponent به شیء window
-  window.MyComponent = MyComponent;
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('@/components/Btnmodal')) :
+  typeof define === 'function' && define.amd ? define(['@/components/Btnmodal'], factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.MyComponent = factory(global.Btnmodal));
+})(this, (function (Btnmodal) { 'use strict';
+
+  function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+  var Btnmodal__default = /*#__PURE__*/_interopDefaultLegacy(Btnmodal);
+
+  function Home() {
+    return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Btnmodal__default["default"], {
+      greeting: "SALAM"
+    }));
+  }
+
+  return Home;
+
+}));
